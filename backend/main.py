01239a5865b4b516c6ab3b476f10bc31d97d4b94
osid54 <osid.potato@gmail.com>
2025-07-20 17:14:28 -0400

    Edited cors to include initial deployment

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,7 @@ app = FastAPI(
 origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    # "https://www.teachers-pet.us",
+    "https://teachers-pet-ashen.vercel.app/",
 ]
 
 app.add_middleware(
